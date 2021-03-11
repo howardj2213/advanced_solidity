0208239d73bf94b83eca56c100fca5d42fe4a993
howardj2213 <howard@edwardsmediasolutions.com>
2021-03-11 13:38:25 -0600

    fixed and compiled code

--- crowdSale.sol
+++ crowdSale.sol
@@ -1,41 +1,18 @@
 pragma solidity ^0.5.0;
 
-import "./PupperCoin.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-// @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
-
+contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
-        // @TODO: Fill in the constructor parameters!
+        string memory name,
+        string memory symbol,
+        uint initial_supply
     )
-        // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        ERC20Detailed(name, symbol, 18)
         public
     {
         // constructor can stay empty
-    }
-}
-
-contract PupperCoinSaleDeployer {
-
-    address public token_sale_address;
-    address public token_address;
-
-    constructor(
-        // @TODO: Fill in the constructor parameters!
-    )
-        public
-    {
-        // @TODO: create the PupperCoin and keep its address handy
-
-        // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-
-        // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
-        token.addMinter(token_sale_address);
-        token.renounceMinter();
     }
 }
